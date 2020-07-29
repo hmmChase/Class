14750327b770e179c42621abcbbf56867826766d
Chase <hmmChase@users.noreply.github.com>
2020-07-28 17:30:23 -0700

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,6 @@ import Layout from './components/Layout/Layout';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 
-function App() {
-  return <Layout header={<Header />} main={<Main />}></Layout>;
-}
+const App = () => <Layout header={<Header />} main={<Main />}></Layout>;
 
 export default App;
