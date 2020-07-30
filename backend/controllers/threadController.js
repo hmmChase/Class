3323b27d1c2e4f2a4506112eb2b496ff9c2a5e4e
Chase <hmmChase@users.noreply.github.com>
2020-07-29 17:27:56 -0700

    Update

--- backend/controllers/threadController.js
+++ backend/controllers/threadController.js
@@ -5,7 +5,11 @@ const prisma = new PrismaClient();
 exports.getThreads = async (req, res, next) => {
   // const id = req.params.id;
 
+  console.log('getThreads');
+
   const threads = await prisma.thread.findMany({ include: { author: true } });
+
+  console.log('threads:', threads);
 
   res.json(threads);
 };
