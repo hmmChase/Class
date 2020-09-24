76afb9e198bb68275916fe7aa09ec6b7315ba669
Chase <hmmChase@users.noreply.github.com>
2020-09-24 08:01:02 -0700

    Update

--- frontend/src/api/baseApi.js
+++ frontend/src/api/baseApi.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { BASE_URL, API_VERSION } from '../config';
 
 export default axios.create({
-  baseURL: 'http://localhost:4000/api/v1',
+  baseURL: `${BASE_URL}/api/${API_VERSION}`,
   withCredentials: true
 });
