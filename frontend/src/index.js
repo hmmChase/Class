b839930a635923aa2596af3bb4d2c396feea8a10
Chase <hmmChase@users.noreply.github.com>
2020-11-07 21:51:46 -0800

    Update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
-
 import { ReactQueryCacheProvider, QueryCache } from 'react-query';
 import { ReactQueryDevtools } from 'react-query-devtools';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global.style';
 import theme from './styles/theme.style';
+
+import * as Sentry from '@sentry/react';
+// import { Integrations } from '@sentry/tracing';
 
 const queryCache = new QueryCache();
 
