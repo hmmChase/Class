822563d3683ea55bd310c175f2937264a503ab97
Chase <hmmChase@users.noreply.github.com>
2020-11-11 17:36:42 -0800

    Update

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
-// import cors from 'cors';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
