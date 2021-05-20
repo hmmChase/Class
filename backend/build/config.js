36b4bb6a5687997ae465cdec0b77e271ec0e124f
Chase <hmmChase@users.noreply.github.com>
2021-05-19 17:03:28 -0700

    Update

--- backend/build/config.js
+++ backend/build/config.js
@@ -8,7 +8,7 @@ var port = process.env.PORT || 4000;
 exports.port = port;
 var COOKIE_CONFIG = {
   maxAge: 365 * 52 * 7 * 24 * 60,
-  domain: 'vercel.app',
+  domain: '.vercel.app',
   httpOnly: process.env.NODE_ENV === 'production',
   secure: process.env.NODE_ENV === 'production' // sameSite: 'none'
   // sameParty: false
--- backend/src/config.js
+++ backend/src/config.js
@@ -2,7 +2,7 @@ export const port = process.env.PORT || 4000;
 
 export const COOKIE_CONFIG = {
   maxAge: 365 * 52 * 7 * 24 * 60,
-  domain: 'vercel.app',
+  domain: '.vercel.app',
   httpOnly: process.env.NODE_ENV === 'production',
   secure: process.env.NODE_ENV === 'production'
   // sameSite: 'none'
